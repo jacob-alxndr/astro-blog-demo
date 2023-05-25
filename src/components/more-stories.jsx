820d04f530326ac4f5893ccbdb0048613fdd1b89
Jacob Martinez <jacobmartinez@Jacobs-MBP.attlocal.net>
2023-05-24 18:30:26 -0700

    feat: updated date format

--- src/components/more-stories.jsx
+++ src/components/more-stories.jsx
@@ -4,7 +4,7 @@ export default function MoreStories({ posts, preview }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Places
+        More Destinations
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-20 gap-y-20 md:gap-y-20 mb-32">
         {posts.map((post) => (
